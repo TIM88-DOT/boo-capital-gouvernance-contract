a9f4811cbf8c2228be6131897066454e05aa27f4
N0name <63561853+TIM88-DOT@users.noreply.github.com>
2022-10-16 13:50:54 +0100

    update

--- boocapital.sol
+++ boocapital.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.13;
@@ -5,25 +6,24 @@ pragma solidity ^0.8.13;
 interface IBooCapitalNFT {
     function ownerOf(uint256) external returns (address);
 
-    function totalSupply() external returns (uint256);
-
     function walletOfOwner(address) external view returns (uint256[] memory);
 }
 
-contract BooCapitalVotes {
-
-    struct Proposal {
+contract BooCapitalVotes is Ownable {
+    struct Contest {
+        Contender[] contenders;
+        bool isRunning;
+    }
+    struct Contender {
         address tokenAddress;
-        string name;
-        string logoUrl;
         uint32 votes;
-        bool inRunning;
     }
 
     uint256 public count;
 
-    mapping(uint256 => Proposal) public proposals;
-    mapping(uint256 => mapping(address => uint256[])) public userNFTsUsedForVote;
+    mapping(uint256 => Contest) public contests;
+    mapping(uint256 => mapping(address => uint256[]))
+        public userNFTsUsedForVote;
 
     IBooCapitalNFT booCapitalNFT;
 
@@ -31,8 +31,66 @@ contract BooCapitalVotes {
         booCapitalNFT = IBooCapitalNFT(_NFTaddress);
     }
 
+    function startBomb(address _firstContenderAddress) public onlyOwner {
+        count += 1;
+        require(contests[count].isRunning == false, "Contest already started");
+        Contender memory newContender = Contender({
+            tokenAddress: _firstContenderAddress,
+            votes: 0
+        });
+        contests[count].contenders.push(newContender);
+        contests[count].isRunning = true;
+    }
+
+    function addContender(uint256 _contestId, address _tokenAddress)
+        public
+        onlyOwner
+    {
+        Contest storage contest = contests[_contestId];
+        Contender memory newContender = Contender({
+            tokenAddress: _tokenAddress,
+            votes: 0
+        });
+        contest.contenders.push(newContender);
+    }
+
+    function vote(
+        uint256 _contestId,
+        uint256 _contenderId,
+        uint256[] memory _userNfts
+    ) public {
+        Contest storage contest = contests[_contestId];
+        uint256 votingPower;
+        require(
+            checkIfNftHasVoted(_userNfts) == false,
+            "NFT(s) already voted with"
+        );
+        require(contest.isRunning == true, "Voting is over");
+        for (uint256 i = 0; i < _userNfts.length; i++) {
+            require(
+                booCapitalNFT.ownerOf(_userNfts[i]) == msg.sender,
+                "You are not owner of the NFT(s) selected"
+            );
+            votingPower++;
+            userNFTsUsedForVote[count][msg.sender].push(_userNfts[i]);
+        }
+        contest.contenders[_contenderId].votes = uint32(votingPower);
+    }
+
+    function endBomb(uint256 _id) public onlyOwner {
+        Contest storage contest = contests[_id];
+        contest.isRunning = false;
+    }
+
+    function getContest(uint256 _id) public view returns (Contest memory) {
+        Contest storage contest = contests[_id];
+        return contest;
+    }
+
+    // internal
+
     function walletOfNFTOwner(address user)
-        external
+        internal
         view
         returns (uint256[] memory)
     {
@@ -40,22 +98,26 @@ contract BooCapitalVotes {
         return id;
     }
 
-    function startBomb(address _tokenAddress, string memory _name, string memory _url) private onlyOwner {
-        count += 1;
-        proposals[count] = Proposal({
-            tokenAddress: _tokenAddress,
-            name: _name,
-            url : _url,
-            votes: 0,
-            inRunning: true
-        });
+    function checkIfNftHasVoted(uint256[] memory _userNfts)
+        public
+        view
+        returns (bool)
+    {
+        uint256[] memory userNFTsUsed = userNFTsUsedForVote[count][msg.sender];
+        bool result;
+        for (uint256 ii = 0; ii < _userNfts.length; ii++) {
+            for (uint256 jj = 0; jj < userNFTsUsed.length; jj++) {
+                if (_userNfts[ii] == userNFTsUsed[jj]) {
+                    result = true;
+                    break;
+                } else {
+                    if (jj == userNFTsUsed.length - 1) {
+                        result = false;
+                        continue;
+                    }
+                }
+            }
+        }
+        return result;
     }
-
-    function vote() public {
-        require(uint(walletOfNFTOwner(msg.sender).length) > 0);
-        uint votingPower = uint(walletOfNFTOwner(msg.sender).length);
-        proposals[count].votes = votingPower;
-    }
-
-    function endBomb() internal {}
 }
